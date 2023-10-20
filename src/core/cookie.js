76f7b7de9265209e7a29d201f04696aeeaf769cf
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-20 12:17:51 -0400

    Update cookie.js

--- src/core/cookie.js
+++ src/core/cookie.js
@@ -1,51 +1,49 @@
-const cookie = () => {
-  return {
-    get(name) {
-      if (typeof name !== 'string') {
-        throw new Error('Get cookie first argument must be of type string.')
-      }
-      try {
-        const cookies = document.cookie.split(';')
-        for (let cookie of cookies) {
-          const [cookieName, ...cookieValueParts] = cookie.trim().split('=')
-          const cookieValue = cookieValueParts.join('=')
-          if (cookieName === name) {
-            return cookieValue
-          }
-        }
-        return undefined 
-      } catch (error) {
-        throw new Error(`Error getting cookie: ${name}`)
-      }
-    },
-    set(name, value, expiration = undefined) {
-      if (typeof name !== 'string') {
-        throw new Error('Set cookie first argument must be of type string.')
-      }
-      if (!value) {
-        throw new Error('Set cookie must have second argument.')
-      }
-      if (expiration && typeof expiration !== 'number') {
-        throw new Error('Set cookie third argument must be a number representing the lifetime of the cookie in seconds.')
-      }
-      const settings = {
-        name: `${name}=${value};`,
-        secure: 'Secure;',
-        samesite: 'SameSite=Strict;',
-        path: 'path=/;'
-      }
-      if (expiration) {
-        settings.maxage = `max-age=${expiration};`
-      }
-      document.cookie = Object.values(settings).join('')
-    },
-    remove(name) {
-      if (typeof name !== 'string') {
-        throw new Error('Remove cookie argument must be of type string.')
-      }
-      document.cookie = `${name}=;max-age=0;path=/;samesite=strict;secure`
+const cookie = {
+  get(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Get cookie first argument must be of type string.')
     }
+    try {
+      const cookies = document.cookie.split(';')
+      for (let cookie of cookies) {
+        const [cookieName, ...cookieValueParts] = cookie.trim().split('=')
+        const cookieValue = cookieValueParts.join('=')
+        if (cookieName === name) {
+          return cookieValue
+        }
+      }
+      return undefined 
+    } catch (error) {
+      throw new Error(`Error getting cookie: ${name}`)
+    }
+  },
+  set(name, value, expiration = undefined) {
+    if (typeof name !== 'string') {
+      throw new Error('Set cookie first argument must be of type string.')
+    }
+    if (!value) {
+      throw new Error('Set cookie must have second argument.')
+    }
+    if (expiration && typeof expiration !== 'number') {
+      throw new Error('Set cookie third argument must be a number representing the lifetime of the cookie in seconds.')
+    }
+    const settings = {
+      name: `${name}=${value};`,
+      secure: 'Secure;',
+      samesite: 'SameSite=Strict;',
+      path: 'path=/;'
+    }
+    if (expiration) {
+      settings.maxage = `max-age=${expiration};`
+    }
+    document.cookie = Object.values(settings).join('')
+  },
+  remove(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Remove cookie argument must be of type string.')
+    }
+    document.cookie = `${name}=;max-age=0;path=/;samesite=strict;secure`
   }
 }
 
-export default cookie()
+export default cookie
